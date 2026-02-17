6109a75522d9ec4948b66c3066a55b6adb7ffeb7
Irella León <ireleon1503@mail.com>
2026-02-16 18:52:19 -0600

    Clase Books

---
+++ Actividad_Aprendizaje_02/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actividad_Aprendizaje_02.Models
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Isbn { get; set; }
+        public int PublicationYear { get; set; }
+        public bool IsLoaned { get; private set; }
+    }
+}
